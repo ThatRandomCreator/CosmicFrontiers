3501f51203ad368d6d0a816ebaf30370898f667d
ThatRandomCreator <donomad800@gmail.com>
2025-12-23 23:54:16 -0500

    Initial commit

---
+++ src/main/java/git/randomcreator/cosmicfrontiers/cosmicfrontiers.java
@@ -0,0 +1,10 @@
+package git.randomcreator.cosmicfrontiers;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(cosmicfrontiers.MODID)
+public class cosmicfrontiers {
+    public static final String MODID = "cosmicfrontiers";
+
+
+}
